212b05d9a1542eaef1f600307c9d904ff2c120b1
ScottAgirs <scott@ijs.to>
2022-02-27 15:02:56 +0200

    chore: rm TODO
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-// TODO: Fix import mapping
 import { Button } from "core"
 
 const Home: NextPage = () => {
