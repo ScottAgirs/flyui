22ce68f1443426cee23cbc1e84e0ac6530afff74
ScottAgirs <scott@ijs.to>
2022-02-27 13:43:48 +0200

    feat(Fly): Init core
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ packages/core/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './data-entry/Button'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Button>Hey Fly</Button>
+    </div>
+  )
+}
+
+export default App
